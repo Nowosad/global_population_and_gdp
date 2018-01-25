3e40dd58a95970434334275d97db632d7e31b0b0
Nowosad <tupiszakaczki@gmail.com>
2018-01-25 12:11:19 -0500

    rasterize -> fasterize

--- R/03_rasterization.R
+++ R/03_rasterization.R
@@ -1,19 +1,21 @@
 library(sf)
 library(tidyverse)
 library(raster)
+# devtools::install_github("ecohealthalliance/fasterize")
+library(fasterize)
 
 bulk_rasterization = function(var, vector, raster_template){
   new_filename = paste0("data/", var, ".tif")
-  rasterize(vector, raster_template, field = var, filename = new_filename)
+  new_raster = fasterize(vector, raster_template, field = var, fun = "last")
+  writeRaster(new_raster, filename = new_filename)
 }
 
 # vector data read --------------------------------------------------------
 regular_grid_data = st_read("data/global_population_and_gdp.gpkg")
 
 # raster template create --------------------------------------------------
-raster_template = raster(extent(regular_grid_data), resolution = 0.5, crs = "+init=epsg:4326")
+my_raster_template = raster(extent(regular_grid_data), resolution = 0.5, crs = "+init=epsg:4326")
 
 # rasterization -----------------------------------------------------------
 colnames(regular_grid_data)[-c(1, 80)] %>% 
-  bulk_rasterization(regular_grid_data, raster_template)
-
+  map(bulk_rasterization, regular_grid_data, my_raster_template)
