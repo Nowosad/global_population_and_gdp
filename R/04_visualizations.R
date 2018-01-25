84ddca3efcd3af0e83bfe7a4d8d38cd05de2a056
Nowosad <tupiszakaczki@gmail.com>
2018-01-25 13:18:50 -0500

    small vis update

--- R/04_visualizations.R
+++ R/04_visualizations.R
@@ -15,3 +15,15 @@ tm1 = tm_shape(p1) +
 
 animation_tmap(tm1)
 
+tm2 = tm_shape(p1) +
+  tm_raster(col = names(p1), style = "fixed", breaks = c(0:12, 60), 
+            palette = tmaptools::get_brewer_pal("Spectral", n = 13),
+            title = paste0("Year ", p1_years, "\n",
+                           "Global population (mln): ", round(p1_sum_pop, 2), "\n",
+                           "Population (mln):")) + 
+  tm_facets(nrow = 1, ncol = 1)
+
+animation_tmap(tm2, "animation2.gif")
+
+
+hist(p1)
