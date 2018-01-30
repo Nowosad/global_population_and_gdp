e0e218670a5cf9871752ba1a72f8c0b41efdc328
jn <tupiszakaczki@gmail.com>
2018-01-29 21:29:51 -0500

    cleaned vector data

--- R/02_regular_grid_recalculate.R
+++ R/02_regular_grid_recalculate.R
@@ -22,12 +22,16 @@ raw_data = st_read("raw_data/grid.shp", stringsAsFactors = FALSE) %>%
 raw_data_pos = st_point_on_surface(raw_data) %>% 
   select(-gID, -ISO3, -px, -py) %>% 
   select(-(p2_1980:p2_2010), -(p3_1980:p3_2010)) %>% 
+  select(-(g2_1980:g2_2010), -(g3_1980:g3_2010)) %>% 
   mutate(grid_id = st_intersects(., regular_grid) %>% flatten_int()) %>% 
   st_set_geometry(NULL)
 
 # update names ------------------------------------------------------------
 p1_estimation_names = c("p1_1980", "p1_1990", "p1_2000", "p1_2010")
 colnames(raw_data_pos)[colnames(raw_data_pos) %in% p1_estimation_names] = c("p_1980", "p_1990", "p_2000", "p_2010")
+
+g1_estimation_names = c("g1_1980", "g1_1990", "g1_2000", "g1_2010")
+colnames(raw_data_pos)[colnames(raw_data_pos) %in% g1_estimation_names] = c("g_1980", "g_1990", "g_2000", "g_2010")
 
 # calculate grouped vars --------------------------------------------------
 raw_data_pos_vars = raw_data_pos %>% 
@@ -38,6 +42,13 @@ raw_data_pos_vars = raw_data_pos %>%
 regular_grid_data = regular_grid %>% 
   left_join(raw_data_pos_vars, by = "grid_id") %>% 
   na.omit()
+
+# clean the data ----------------------------------------------------------
+regular_grid_data = regular_grid_data %>% 
+  select(starts_with("p_"), starts_with("g_"), 
+         starts_with("p1_"), starts_with("g1_"),
+         starts_with("p2_"), starts_with("g2_"),
+         starts_with("p3_"), starts_with("g3_"))
 
 # save new data -----------------------------------------------------------
 dir.create("data")
